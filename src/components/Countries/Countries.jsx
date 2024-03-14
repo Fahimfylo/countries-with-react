881d90842fa38670edd6cf813a3cea5cc98a5d56
unknown <ghostpy91@gmail.com>
2024-03-15 03:59:28 +0600

    added country list

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,23 +1,44 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../Country/Country";
+import './Countries.css'
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
+    const [visitedCountries, setvisitedCountries] = useState([])
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
-        .then(res => res.json())
-        .then(data => setCountries(data));
-    },[])
+            .then(res => res.json())
+            .then(data => setCountries(data));
+    }, [])
+
+    const handleVisitedCountry = country =>{
+        console.log('add this to your visited country')
+        const newVisitedCountries =[...visitedCountries,country];
+        setvisitedCountries(newVisitedCountries);
+    }
 
     return (
         <div>
             <h3>Countries : {countries.length}</h3>
-            {
-                countries.map(country =>
-                <Country key={country.cca3} country={country}></Country>)
-            }
+            <div>
+                <h5>Visited Countries : {visitedCountries.length}</h5>
+                <ul>
+                    {
+                        visitedCountries.map(country => <li key={country.cca3}>{country.name.common}</li>)
+                    }
+                </ul>
+            </div>
+
+            <div className="country-layout">
+                {
+                    countries.map(country => <Country
+                        key={country.cca3}
+                        handleVisitedCountry ={handleVisitedCountry}
+                        country={country} ></Country>)
+                }
+            </div>
         </div>
     );
 };
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,14 +1,26 @@
+import { useState } from 'react';
 import './Country.css'
-const Country = ({country}) => {
-    const {name, flags} = country;
+const Country = ({ country , handleVisitedCountry}) => {
+    const {name, flags, population , area ,cca3} = country;
+    const [visited, setVisited] = useState(false)
+
+    const handleVisited = () => {
+        setVisited(!visited);
+    }
+
     
     return (
-        <div className='country'>
-            <h3>Name : {name.common}</h3>
-            <img src={flags.png} alt=""/>
-
+        <div className={`country ${visited && 'visited'}`}>
+            <h3 style={{color: visited ? 'red' : 'black'}}>{name.common}</h3>
+            <img src={flags.png} alt="" />
+            <p>Population : {population}</p>
+            <p>Area : {area}</p>
+            <p><small>Code : {cca3}</small></p>
+            <button onClick={() => handleVisitedCountry(country)}>Mark Visited</button>
+            <br />
+            <button onClick={handleVisited}>{visited ? 'visited' : 'going'}</button>
+            {visited ? 'Already visited' : 'want to visit'}
         </div>
-    );
-};
-
-export default Country;
+    )
+}
+ export default Country;
